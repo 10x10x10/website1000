7f76b2507712b3578863037ccd2312fa0187f89b
ttes93405 <ttesttes93405@gmail.com>
2021-05-25 01:43:21 +0800

    將header抽成component

--- js/main.js
+++ js/main.js
@@ -1,6 +1,49 @@
 
+const header = Vue.component('header-component', {
+     props: ['root'],
+     template: '\
+     <div class="header">\
+         <div class="title-conatainer">\
+             <h1 class="title">{{title}}</h1>\
+         </div>\
+         <div class="flex-space"></div>\
+         <div class="header-link-container">\
+             <a class="header-link" v-for="link in getLinks" v-bind:href="link.link">{{link.title}}</a>\
+         </div>\
+     </div>\
+     ',
+     data: function () {
+     return {
+         title: "1000 Cheng",
+         links: [
+             { 
+               title: "HOME",
+               link: "index.html"
+             }, 
+             { 
+               title: "LAB",
+               link: "lab.html"
+             }, 
+             { 
+               title: "ABOUT",
+               link: "about.html"
+             },
+         ]
+     }
+     },
+     computed: {
+         getLinks: function(){
+             let root = (this.root===undefined) ? "" : this.root;
+             return this.links.map(n=>{
+                 n.link = root + n.link;
+                 return n;
+             });
+         }
+     }
+ });
+ 
 
-var app = new Vue({
+const app = new Vue({
     el: '#app',
     data: {
         homeData: [
