c9a9a62a68f9a64af088da6bf1650d0242f2ccaa
OwenSun <ttesttes93405@gmail.com>
2021-08-12 22:44:18 +0800

    加 Google Analytics

--- pages/components/HeadMeta.js
+++ pages/components/HeadMeta.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from 'next/head'
+import HeadGoogleAnalytics from './HeadGoogleAnalytics.js';
 import { metaData, projectData } from "../../data.js";
 
 export default function HeadMeta({ type, id }) {
@@ -38,17 +39,20 @@ export default function HeadMeta({ type, id }) {
   };
 
   return (
-    <Head>
-      <title>{resultMetaData.title}</title>
-      <link rel="icon" href={resultMetaData.icon} />
-      <meta name="description" content={resultMetaData.description} />
-      <meta name="author" content={resultMetaData.author} />
-      <meta property="og:type" content={resultMetaData.ogType} />
-      <meta property="og:title" content={resultMetaData.title} />
-      <meta property="og:description" content={resultMetaData.description} />
-      <meta property="og:image" content={resultMetaData.ogImage} />
-      <meta property="og:site_name" content={resultMetaData.ogSiteName} />
-      <meta property="og:url" content={resultMetaData.ogUrl} />
-    </Head>
+    <>
+      <HeadGoogleAnalytics />
+      <Head>
+        <title>{resultMetaData.title}</title>
+        <link rel="icon" href={resultMetaData.icon} />
+        <meta name="description" content={resultMetaData.description} />
+        <meta name="author" content={resultMetaData.author} />
+        <meta property="og:type" content={resultMetaData.ogType} />
+        <meta property="og:title" content={resultMetaData.title} />
+        <meta property="og:description" content={resultMetaData.description} />
+        <meta property="og:image" content={resultMetaData.ogImage} />
+        <meta property="og:site_name" content={resultMetaData.ogSiteName} />
+        <meta property="og:url" content={resultMetaData.ogUrl} />
+      </Head>
+    </>
   );
 }
