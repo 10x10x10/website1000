51bc74f5c246fd05f72c2c3fdc4f03b51887fcfb
OwenSun <ttesttes93405@gmail.com>
2021-08-24 22:08:42 +0800

    - 新增PoweredBy - 修正在較舊的 iOS 上會顯示錯誤的問題

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { footerData } from "../../data.js";
+import PoweredBy from "./PoweredBy.js";
 
 export default function Header(props) {
 
@@ -7,18 +8,24 @@ export default function Header(props) {
 
   return (
     <div className="footer">
+
       <div className="link-icon-container">
         {
           links.map((item) => {
             return (
-              <a href={item.link} key={item.link}>
+              <a href={item.link} key={item.link} target="_blank">
                 <img src={item.icon} className="icon" />
               </a>);
           })
         }
       </div>
       <p>{description}</p>
+
+      <div style={{height: 10}} />   {/* split space */}
+
       <p>{copyright}</p>
+      <PoweredBy />
+
     </div>
   );
 }
---
+++ pages/components/PoweredBy.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function PoweredBy(props) {
+
+  const text = `© 2021 Website powered by `;
+
+  const owenWebsiteUrl = "https://ttesttes93405.github.io/OwenCollections/";
+  const owen = (<a
+    href={owenWebsiteUrl}
+    target="_blank"
+  >Owen Sun</a>)
+
+  return (
+    <div className="powered-by">
+      <p>{text}{owen}</p>
+    </div>
+  );
+}
