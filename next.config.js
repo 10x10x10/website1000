e45d6b86bf93315cabb1f5ab55299082118558aa
1000Cheng <10x10x10w1000@gmail.com>
2024-02-01 13:58:54 +0800

    chore: 加回遺漏設置

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   reactStrictMode: true,
   assetPrefix: isProd ? "" : "",
+  output: 'export',
   compiler: {
     // styledComponents: true,
   },
